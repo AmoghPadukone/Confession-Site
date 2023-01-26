f5cdcc905bbc49ae36061bd4c229cc90f3900192
Amogh <35802992+AmoghPadukone@users.noreply.github.com>
2023-01-27 01:15:10 +0530

    Update firebaseConfig.js
    removed sensitive info

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,13 +9,13 @@ import { collection } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIdE6J9oHvV1OPu3xY6KMNh1t7bDuHPwU",
-  authDomain: "confession-site-2023.firebaseapp.com",
-  projectId: "confession-site-2023",
-  storageBucket: "confession-site-2023.appspot.com",
-  messagingSenderId: "895708120883",
-  appId: "1:895708120883:web:2fd8a1d898001cb9402046",
-  measurementId: "G-HD3E8VW40Z",
+  apiKey: "#YOUR_API_KEY",
+  authDomain: "#AUTH_DOMAIN",
+  projectId: "#PROJECT_ID",
+  storageBucket: "#",
+  messagingSenderId: "#",
+  appId: "#",
+  measurementId: "#",
 };
 
 // Initialize Firebase
